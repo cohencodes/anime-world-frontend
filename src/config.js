ea44ef1a03b86856f58de5895d63eb5570f414f5
Alon Cohen <alon@cohencodes.com>
2019-06-26 11:28:20 -0700

    config fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   API_ENDPOINT:
-    'https://anime-world-cohencodes.herokuapp.com/' ||
+    'https://anime-world-cohencodes.herokuapp.com/api' ||
     'http://localhost:8000/api',
   GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
   TOKEN_KEY: 'anime-client-auth-token'
