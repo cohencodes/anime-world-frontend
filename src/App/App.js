a165b5bcf9267c4e46fd8c7d680c53c2a0bdac48
Alon Cohen <alon@cohencodes.com>
2019-06-26 18:14:41 -0700

    initial commit from router-restructure

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import '../index.css';
 import Navbar from '../components/Navbar/Navbar';
-import routes from '../services/routes';
-import Footer from '../components/Footer/Footer';
+import LandingPage from '../components/LandingPage/LandingPage';
+import SignupForm from '../components/SignupForm/SignupForm';
+import Login from '../components/Login/Login';
+import Search from '../components/Search/Search';
+import DetailPage from '../components/DetailPage/DetailPage';
 
 class App extends Component {
   render() {
@@ -12,16 +15,12 @@ class App extends Component {
         <Navbar />
         <main role="main">
           <Switch>
-            {routes.map(({ path, component: C, fetchInitialData }) => (
-              <Route
-                exact
-                path={path}
-                render={props => <C {...props} data={fetchInitialData} />}
-                key={path}
-              />
-            ))}
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/signup" component={SignupForm} />
+            <Route path="/login" component={Login} />
+            <Route path="/search" component={Search} />
+            <Route path="/detailpage" component={DetailPage} />
           </Switch>
-          <Footer />
         </main>
       </>
     );
